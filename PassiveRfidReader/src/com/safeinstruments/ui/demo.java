01195a6199aa9bd5d8e896640bd8077acc3072b3
gurpreet <gslcet@gmail.com>
2014-12-25 18:42:57 +0530

    added passive card reader

---
+++ PassiveRfidReader/src/com/safeinstruments/ui/demo.java
@@ -0,0 +1,14 @@
+package com.safeinstruments.ui;
+
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("com.safeinstruments.ui/PassiveReaderConfig");
+		System.out.println(bundle.getString(""));
+	}
+
+}
